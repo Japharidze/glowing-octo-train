31ee269b3f2aac407e2e872b5dba2d560c736ce6
Japharidze <sergi.japharidze@gmail.com>
2022-01-15 14:51:44 +0400

    config: add heroku-postgres url

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sulertia'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or 'sqlite:///bot/database.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') \
+        or 'sqlite:///bot/database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
